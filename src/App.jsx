21a3f7dcb6c1af1fcd7230e852e2b0a3cb15effd
Vimalprasath <vimalprasath2000@gmail.com>
2026-01-14 18:41:13 +0530

    Setup VITE, Tailwind CSS and Favicon Changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center">Let's cook Netflix GPT</h1>
+    </>
+  );
+}
+
+export default App;
